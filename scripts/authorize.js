5eeba7257b9250cdb9f7946f2171ba0bd4cc4589
huichang <yhc122833@pukyong.ac.kr>
2025-01-24 15:47:45 +0900

    modify repository save directory

--- scripts/authorize.js
+++ scripts/authorize.js
@@ -11,8 +11,8 @@ const localAuth = {
     this.KEY = 'leethub_token';
     this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
     this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
-    this.CLIENT_ID = '0114dd35b156d4729fac';
-    this.CLIENT_SECRET = 'cfc3301d9745530bf1b31e92528ad9c31fd3f995';
+    this.CLIENT_ID = '';
+    this.CLIENT_SECRET = '';
     this.REDIRECT_URL = 'https://github.com/';
     this.SCOPES = ['repo'];
   },
